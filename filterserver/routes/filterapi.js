b7ebfad598ff5f0b4d89cf080995ff9ceb3460d7
shwethag <g.shwetha.g@gmail.com>
2015-04-07 01:27:55 +0530

    added api to get loation based data

--- filterserver/routes/filterapi.js
+++ filterserver/routes/filterapi.js
@@ -10,11 +10,38 @@ router.route('/getdata').get(function(req, res) {
   	{sensorId : req.query.sensorId},
   	function(err, sensordata) {
     if (err) {
-      return res.send(err);
+      return res.send("error in request");
     }
  
     res.json(sensordata);
   });
+});
+
+router.route('/getdata/geolocation').post(function(req, res) {
+  console.log(req.body.latitude);
+  console.log(req.body.longitude);
+  if(req.body.radius == undefined){
+    console.log('radius is undefined ');
+    filterapi.find({
+        $and: [
+         {"location.latitude" : req.body.latitude},
+         {"location.longitude": req.body.longitude}
+        ]},
+        function(err, sensordata) {
+        if (err) {
+          return res.send(err);
+        }
+           res.json(sensordata);
+       
+       });
+     
+    }
+    else
+    {
+      console.log('radius is ' + req.body.radius);
+      res.json({"good" : "ok"});
+    }
+   
 });
 
 router.route('/sensordata').post(function(req, res) {
